bf62fdb4b2f1c18519b8976a80f7ae95a8382071
djamal lamri <contact@progfacil.com>
2021-09-24 15:44:46 +0200

    templite

---
+++ portfolio.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <title> exos 1 </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+
+    <body>
+        <header><?php include 'menu.html'; ?></header>
+            
+        <h1>Mon portfolio</h1>
+        <p>Mon ici la</p>
+
+        <footer><?php include "footer.html"; ?> </footer>
+    </body>
+
+</html>
